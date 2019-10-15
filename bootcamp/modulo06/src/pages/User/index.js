0533fe27fa9eab377c8cb0bcd1ca0be952a84ddf
bhborba <brunohenriqueborba@gmail.com>
2019-10-15 06:00:37 -0300

    Adicionado activity indicator sem estilizacao

--- bootcamp/modulo06/src/pages/User/index.js
+++ bootcamp/modulo06/src/pages/User/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {ActivityIndicator} from 'react-native';
 import api from '../../services/api';
 
 import {
@@ -14,6 +15,7 @@ import {
   Info,
   Title,
   Author,
+  Loading,
 } from './styles';
 
 export default class User extends Component {
@@ -29,6 +31,7 @@ export default class User extends Component {
 
   state = {
     stars: [],
+    loading: true,
   };
 
   async componentDidMount() {
@@ -36,12 +39,16 @@ export default class User extends Component {
     const user = navigation.getParam('user');
 
     const response = await api.get(`/users/${user.login}/starred`);
-    this.setState({stars: response.data});
+
+    this.setState({
+      stars: response.data,
+      loading: false,
+    });
   }
 
   render() {
     const {navigation} = this.props;
-    const {stars} = this.state;
+    const {stars, loading} = this.state;
 
     const user = navigation.getParam('user');
     return (
@@ -51,19 +58,23 @@ export default class User extends Component {
           <Name>{user.name}</Name>
           <Bio>{user.bio}</Bio>
         </Header>
-        <Stars
-          data={stars}
-          keyExtractor={star => String(star.id)}
-          renderItem={({item}) => (
-            <Starred>
-              <OwnerAvatar source={{uri: item.owner.avatar_url}} />
-              <Info>
-                <Title>{item.name}</Title>
-                <Author>{item.owner.login}</Author>
-              </Info>
-            </Starred>
-          )}
-        />
+        {loading ? (
+          <ActivityIndicator color="#000" />
+        ) : (
+          <Stars
+            data={stars}
+            keyExtractor={star => String(star.id)}
+            renderItem={({item}) => (
+              <Starred>
+                <OwnerAvatar source={{uri: item.owner.avatar_url}} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
       </Container>
     );
   }
