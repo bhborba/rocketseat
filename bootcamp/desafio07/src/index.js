8438f170592837a23a9ce9c612c269a062b78e8e
bhborba <brunohenriqueborba@gmail.com>
2019-12-29 00:00:51 -0300

    React Navigation

--- bootcamp/desafio07/src/index.js
+++ bootcamp/desafio07/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-
 import { View, Text } from 'react-native';
 
 import './config/ReactotronConfig';
 
+import Routes from './routes';
+
 export default function App() {
-    return (
-        <View>
-            <Text>Opa</Text>
-        </View>
-    );
+    return <Routes />;
 }
---
+++ bootcamp/desafio07/src/routes.js
@@ -0,0 +1,14 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+
+const Routes = createAppContainer(
+    createStackNavigator({
+        Home,
+        Cart,
+    })
+);
+
+export default Routes;
