fd179758186c726dff049f7ef0256354cb34f9dd
bhborba <brunohenriqueborba@gmail.com>
2019-12-28 23:36:38 -0300

    Configurando Reactotron

--- bootcamp/desafio07/.eslintrc.js
+++ bootcamp/desafio07/.eslintrc.js
@@ -1,34 +1,40 @@
 module.exports = {
-  env: {
-    es6: true,
-  },
-  extends: [
-    'airbnb',
-    'prettier',
-    'prettier/react',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+      browser: true,
+      es6: true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    'prettier',
-  ],
-  rules: {
-      'prettier/prettier' : 'error',
-      'react/jsx-filename-extension': [
-        'warn',
-        { extensions: ['.jsx','.js'] }
+    extends: [
+      'airbnb',
+      'prettier',
+      'prettier/react'
     ],
-    'import/prefer-default-export': 'off',
-  },
-};
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+      __DEV__ : 'readonly',
+    },
+    parser: 'babel-eslint',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+      'prettier',
+    ],
+    rules: {
+        'prettier/prettier': 'error',
+        'react/jsx-filename-extension': [
+            'warn',
+            { extensions: ['.jsx','.js'] }
+        ],
+        'import/prefer-default-export': 'off',
+        'no-param-reassign': 'off',
+        'react/state-in-constructor': 'off',
+        'react/static-property-placement': 'off',
+        'react/jsx-props-no-spreading': 'off',
+    },
+  };
