f1dfaa484e804ef30c4a64ddc7f95e0866e7f792
bhborba <brunohenriqueborba@gmail.com>
2020-01-31 12:57:32 -0300

    Gerando dados aleatorios

--- bootcamp/modulo11/server/src/app/models/User.js
+++ bootcamp/modulo11/server/src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,6 +7,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       {
@@ -13,6 +15,11 @@ class User extends Model {
       }
     );
 
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
     return this;
   }
 }
